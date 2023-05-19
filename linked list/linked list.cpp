46161a43399226fd3a0ef42b7e149b849082fa32
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-19 19:36:11 +0700

    case 2

--- linked list/linked list.cpp
+++ linked list/linked list.cpp
@@ -140,4 +140,20 @@ int main()
 
 		}
 		break;
+		case '2':
+		{
+			if (mhs.listempty())
+			{
+				cout << endl << "List Kosong" << endl;
+				break;
+			}
+			cout << endl << "\nMasukkan no mahasiswa yang akan dihapus : ";
+			cin >> nim;
+			if (mhs.delNode(nim) == false)
+				cout << endl << "Data tidak ditemukan " << endl;
+			else
+				cout << endl << "Data dengan nomor mahasiswa " << nim << "berhasil dihapus";
+
+		}
+		break;
 		
