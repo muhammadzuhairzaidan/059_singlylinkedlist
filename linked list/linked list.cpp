faea3ba0be1015b8370456abaf6037ac58083211
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-19 19:19:16 +0700

    membuat class list

--- linked list/linked list.cpp
+++ linked list/linked list.cpp
@@ -9,3 +9,15 @@ public:
 	char nama[20];
 	Node* next;
 };
+class List
+{
+	Node* START;
+public:
+	List();
+	void addNode();
+	bool search(int nim, Node** current, Node** previous);
+	bool listempty();
+	bool delNode(int element);
+	void traverse();
+};
+
