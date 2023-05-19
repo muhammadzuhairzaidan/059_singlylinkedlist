7b8dacbc75a39487c3d1679854560bab9bbbed39
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-19 19:33:47 +0700

    menampilkan kalimat

--- linked list/linked list.cpp
+++ linked list/linked list.cpp
@@ -125,4 +125,13 @@ int main()
 	char ch;
 	while (1)
 	{
+		cout << endl << "Menu";
+		cout << endl << "1. Menambahkan data kedalam list" << endl;
+		cout << "2. Menghapus data dari dalam list" << endl;
+		cout << "3. Menampilkan semua data didalam list" << endl;
+		cout << "5. Keluar" << endl;
+		cout << endl << "Masukkan piliha (1-5): ";
+		cin >> ch;
+		switch (ch)
+		{
 		
