829a7cda8a72fbaa795d12a377efdca53d1b1842
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-19 19:23:53 +0700

    prosedur List tambah elemen

--- linked list/linked list.cpp
+++ linked list/linked list.cpp
@@ -20,4 +20,57 @@ public:
 	bool delNode(int element);
 	void traverse();
 };
+List::List()
+{
+	START = NULL;
+}
+
+void List::addNode()
+{
+	int nim;
+	char nm[20];
+	cout << "\nMasukkan Nomor Mahasiswa: ";
+	cin >> nim;
+	cout << "\nMasukkan Nama: ";
+	cin >> nm;
+
+	Node* nodeBaru = new Node;
+	nodeBaru->noMhs = nim;
+	strcpy_s(nodeBaru->nama, nm);
+	if (START == NULL || nim <= START->noMhs)
+	{
+		if ((START != NULL) && (nim == START->noMhs))
+		{
+			cout << "\nDuplikasi noMhs tidak diizinkan\n";
+			return;
+		}
+		nodeBaru->next = START;
+		START = nodeBaru;
+		return;
+	}
+
+	Node* previous, * current;
+
+
+	current = START;
+	previous = START;
+
+
+
+	while ((current != NULL) && (nim >= current->noMhs))
+	{
+		if (nim == current->noMhs)
+		{
+			cout << "\nDuplikasi noMhs tidak diizinkan\n";
+			return;
+		}
+		previous = current;
+		current = current->next;
+
+	}
+	/*Jika loop diatas dieksekusi, previous dan current akan menempati posisi dimana */
+	nodeBaru->next = current;
+	previous->next = nodeBaru;
+}
+
 
