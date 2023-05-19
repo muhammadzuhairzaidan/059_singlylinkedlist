aa853a893679c562cc900ab000d00899ea047f0f
Muhammad Zuhair Zaidan <muhammad.zuhair.ft22@mail.umy.ac.id>
2023-05-19 19:30:47 +0700

    prosedur void menampilkan data pada list

--- linked list/linked list.cpp
+++ linked list/linked list.cpp
@@ -92,4 +92,29 @@ bool List::delNode(int nim)
 	return true;
 }
 /*Check apakah node yang dimaksud ada didalam list atau tidak*/
-
+bool List::search(int nim, Node** previous, Node** current)
+{
+	*previous = START;
+	*current = START;
+	while ((*current != NULL) && (nim != (*current)->noMhs))
+	{
+		*previous = *current;
+		*current = (*current)->next;
+	}
+	return (*current != NULL);
+}
+void List::traverse()
+{
+	if (listempty())
+		cout << "\nList Kosong\n";
+	else
+	{
+		cout << endl << "Data didalam list adalah:" << endl;
+		Node* currentNode;
+		for (currentNode = START; currentNode != NULL; currentNode = currentNode->next)
+		{
+			cout << currentNode->noMhs << "  " << currentNode->nama << "\n";
+		}
+		cout << endl;
+	}
+}
